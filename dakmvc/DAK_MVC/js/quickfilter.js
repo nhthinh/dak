2731414a251a821674cad2fb99dcca3c0692329a
Hai Hoang <hoangqh90@gmail.com>
2021-08-09 22:32:15 +0700

    fix gui

--- dakmvc/DAK_MVC/js/quickfilter.js
+++ dakmvc/DAK_MVC/js/quickfilter.js
@@ -1,4 +1,6 @@
-﻿function ThanhPhoFilter_Focus() {
+﻿function ThanhPhoFilter_Focus(o) {
 
     $('#divPartialThanhPhoFilter').load('/DAK/ListCity');
-}
+    $(o).closest('.input-list-checkbox').find('.list-checkbox-defaut').show();
+
+ }
